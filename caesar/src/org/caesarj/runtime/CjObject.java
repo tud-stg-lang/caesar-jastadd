763112c7d6ab2feb2ac4ef8884667b767bb9fe7b
Marko Martin <MarkoMartin@gmx.net>
2011-11-07 22:25:07 +0100

    Removed transformation to init methods

--- caesar/src/org/caesarj/runtime/CjObject.java
+++ caesar/src/org/caesarj/runtime/CjObject.java
@@ -1,16 +1,7 @@
 package org.caesarj.runtime;
 
 public class CjObject implements CjObjectIfc {
-	protected CjObjectIfc $cj$family;
-	
-	public CjObject(CjObjectIfc family) {
-		$cj$family = family; 
-	}
-	
-	public CjObject $cj$init() {
-		/* just to be available for super calls */
-		return this;
-	}
+	protected CjObjectIfc $cj$family = null; // TODO remove?
 	
 	public CjObjectIfc cjFamily() {
 		return $cj$family;
--- caesar/src/org/caesarj/runtime/CjObjectIfc.java
+++ caesar/src/org/caesarj/runtime/CjObjectIfc.java
@@ -1,8 +1,6 @@
 package org.caesarj.runtime;
 
 public interface CjObjectIfc {
-	public CjObject $cj$init();
-	
 	public CjObjectIfc cjFamily();
 	
 	public RuntimeCaesarClass getRuntimeClass();
