576aa16c494f0d980a00bd9133f509b292a1dbd2
Marko Martin <MarkoMartin@gmx.net>
2011-11-20 18:56:23 +0100

    * Solved problems with inner classes without init methods * Removed unused test cases

--- caesar/src/org/caesarj/runtime/CjObject.java
+++ caesar/src/org/caesarj/runtime/CjObject.java
@@ -1,21 +1,25 @@
 package org.caesarj.runtime;
 
 public class CjObject implements CjObjectIfc {
-	protected CjObjectIfc $cj$family = null; // TODO remove?
-	
+
+	protected CjObjectIfc $cj$family;
+
+	public CjObject(CjObjectIfc family) {
+		$cj$family = family;
+	}
+
 	public CjObjectIfc cjFamily() {
 		return $cj$family;
 	}
-	
+
 	public RuntimeCaesarClass getRuntimeClass() {
 		try {
-			return (RuntimeCaesarClass)RuntimeClass.forClass(getClass());
-		}
-		catch (RuntimeTypeException e) {
+			return (RuntimeCaesarClass) RuntimeClass.forClass(getClass());
+		} catch (RuntimeTypeException e) {
 			throw new RuntimeInconsistencyError(e.getMessage());
-		}
-		catch (ClassCastException e) {
+		} catch (ClassCastException e) {
 			throw new RuntimeInconsistencyError(e.getMessage());
 		}
 	}
+	
 }
